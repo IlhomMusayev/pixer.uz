11713312362b956e4c26f075438c261e8038fdec
IlhomMusayev <ilhommusayev2001@gmail.com>
2022-02-04 19:38:31 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Pixer.uz</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
